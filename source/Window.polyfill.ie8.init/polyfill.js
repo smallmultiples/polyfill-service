472eb7c1f53c61daa48f4a6606dc875a41887500
James Hiscock <james@small.mu>
2014-07-11 13:47:40 +1000

    Fixed IE8 cloneNode and innerHTML for table and all readonly dom elements.

--- source/Window.polyfill.ie8.init/polyfill.js
+++ source/Window.polyfill.ie8.init/polyfill.js
@@ -5,20 +5,36 @@
 		document.createElement(nodeName);
 	});
 
+	var readOnly = [
+		'COL', 'COLGROUP', 'FRAMESET', 'HEAD', 'HTML', 'STYLE', 'TABLE',
+		'TBODY', 'TFOOT', 'THEAD', 'TITLE', 'TR'
+	];
+
+	var noCloneOverride = [
+		'TBODY', 'TFOOT', 'THEAD', 'TR', 'COL', 'COLGROUP', 'TD', 'TH'
+	]
+
+	var nodeCloner = Object.getOwnPropertyDescriptor(Element.prototype, 'cloneNode').value
+
 	// Element.prototype.cloneNode
 	Object.defineProperty(Element.prototype, 'cloneNode', {
 		value: function (deep) {
-			var
-			element = this,
-			xElement = this.document.createElement('x');
-
-			xElement.innerHTML = element.outerHTML;
-
-			if (!deep) {
-				xElement.firstChild.innerHTML = '';
+			var element = this;
+			if (noCloneOverride.indexOf(element.tagName) !== -1) {
+				return nodeCloner.call(element, deep)
 			}
+			else {
+				var xElement = this.document.createElement('x');
+				xElement.innerHTML = element.outerHTML;
 
-			return xElement.removeChild(xElement.firstChild);
+				if (!deep) {
+					if (readOnly.indexOf(xElement.firstChild.tagName) !== -1) {
+						xElement.firstChild.innerHTML = '';
+					}
+				}
+
+				return xElement.removeChild(xElement.firstChild);
+			}
 		}
 	});
 
@@ -28,6 +44,9 @@
 	Object.defineProperty(Element.prototype, 'innerHTML', {
 		set: function (value) {
 			var element = this;
+			if (readOnly.indexOf(element.tagName) !== -1) {
+				return
+			}
 
 			String(value).replace(/<\w+/g, function (tagName) {
 				element.document.createElement(tagName.slice(1));
